02cc0854f6370581f374ea54ccc7663dfe8b035a
williamswarren <warrenwilliams1996@gmail.com>
2021-04-23 01:45:38 -0700

    Getting familiar with OpenCV

---
+++ opencv_tests/contours.py
@@ -0,0 +1,22 @@
+'''
+import numpy as np
+import cv2
+
+img = cv2.imread('./test_graph.jpg', cv2.IMREAD_GRAYSCALE)
+ret,thresh = cv2.threshold(img,127,255,0)
+#image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+contours,hierachy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+#cnt = contours[4]
+#img = cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
+'''
+import numpy as np
+import cv2 as cv
+im = cv.imread('BINARY_INV.jpg')
+imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+im = cv.drawContours(im, contours, -1, (0,255,0), 3)
+cv.imwrite('contour_graph.jpg',im)
+cv.imshow('image',im)
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ opencv_tests/line_script.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('./test_graph.jpg',0)
+'''
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+'''
+
+edges = cv2.Canny(img,190,200)
+cv2.imwrite('edge_graph.jpg',edges)
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
